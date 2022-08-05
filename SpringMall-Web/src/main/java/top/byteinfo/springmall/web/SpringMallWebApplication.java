363bba57613e7ca79b358d4f6aa8447516acfe4e
xcv <ecsexecxcv@gmail.com>
2022-08-06 07:31:09 +0800

    init

---
+++ SpringMall-Web/src/main/java/top/byteinfo/springmall/web/SpringMallWebApplication.java
@@ -0,0 +1,13 @@
+package top.byteinfo.springmall.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMallWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMallWebApplication.class, args);
+    }
+
+}
---
+++ SpringMall-Web/src/test/java/top/byteinfo/springmall/web/SpringMallWebApplicationTests.java
@@ -0,0 +1,13 @@
+package top.byteinfo.springmall.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMallWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
