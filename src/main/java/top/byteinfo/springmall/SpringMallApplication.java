b2be1867c40d10baed40add57d7d084d8584d3fc
xcv <ecsexecxcv@gmail.com>
2022-08-06 06:44:17 +0800

    init

---
+++ src/main/java/top/byteinfo/springmall/SpringMallApplication.java
@@ -0,0 +1,13 @@
+package top.byteinfo.springmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMallApplication.class, args);
+    }
+
+}
